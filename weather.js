31cf13a74bb066faf07ce9ebe6535d057d207f0a
Rajat Pandit <rahulpandit106@gmail.com>
2019-06-26 16:27:43 +0530

    second commit

---
+++ weather.js
@@ -0,0 +1,82 @@
+var forecast=[{
+      
+         "Bengaluru": {
+            "description": "Isolated Thunderstorm",
+            "maxTemp": 30,
+            "minTemp": 21,
+            "windspeed": 16,
+            "danger": false
+         }
+      ,
+      
+         "Mumbai": {
+            "description": "Haze",
+            "maxTemp": 37,
+            "minTemp": 28,
+            "windspeed": 11,
+            "danger": false
+         }
+      ,
+      
+         "Delhi": {
+            "description": "Haze",
+            "maxTemp": 35,
+            "minTemp": 29,
+            "windspeed": 5,
+            "danger": false
+         }
+      ,
+      
+         "Chennai": {
+            "description": "Mostly Cloudy",
+            "maxTemp": 38,
+            "minTemp": 28,
+            "windspeed": 16,
+            "danger": false
+         }
+      ,
+      
+         "Hyderabad": {
+            "description": "Mostly Cloudy",
+            "maxTemp": 34,
+            "minTemp": 24,
+            "windspeed": 3,
+            "danger": false
+         }
+      ,
+      
+         "Kolkata": {
+            "description": "Mostly Cloudy",
+            "maxTemp": 38,
+            "minTemp": 28,
+            "windspeed": 23,
+            "danger": false
+         }
+      ,
+      
+         "Kerala": {
+            "description": "Haze",
+            "maxTemp": 30,
+            "minTemp": 26,
+            "windspeed": 10,
+            "danger": false
+         }
+}
+   ]
+   // function getRandomInt(max) {
+   //    return Math.floor(Math.random() * Math.floor(max));
+   //  }
+  
+   function dynam(){
+      var forcst= document.getElementById("weather");
+       console.log(forcst)
+   for(var k in forecast[0]){
+      console.log([k]);
+      setTimeout(()=>{
+         forcst.innerHTML=JSON.stringify(forecast[0][JSON.stringify(k)]);
+            },3000)
+      
+   }
+   
+}
+dynam();
